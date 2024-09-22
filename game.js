e2ddf78034cc33c7cac1c232acbf26fcd03ce8ee
Birat Roka <146309394+birat04@users.noreply.github.com>
2024-09-22 21:38:47 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,172 @@
+let allBox = document.querySelectorAll(".box");
+let reset_btn = document.querySelector("#btn");
+let winner = document.querySelector(".winner");
+
+let first0 = true;
+
+const winningPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+allBox.forEach((box) => {
+  box.addEventListener("click", () => {
+    console.log("box was clicked");
+    if (first0) {
+      box.innerText = "O";
+      first0 = false;
+    } else {
+      box.innerText = "X";
+      first0 = true;
+    }
+    box.disabled = true;
+
+    checkWinner();
+  });
+});
+
+function dis() {
+  allBox.forEach((box) => {
+    winner.innerText = `winner is ${first0 ? "X" : "O"}`;
+    box.disabled = true;
+  });
+}
+function reset() {
+  allBox.forEach((box) => {
+    winner.innerText = `winner is ${first0 ? "X" : "O"}`;
+    box.innerText = "";
+    box.disabled = false;
+  });
+  winner.innerText = "";
+}
+
+const checkWinner = () => {
+  for (pattern of winningPatterns) {
+    let posVal1 = allBox[pattern[0]].innerText;
+    let posVal2 = allBox[pattern[1]].innerText;
+    let posVal3 = allBox[pattern[2]].innerText;
+
+    if (posVal1 != "" && posVal2 != "" && posVal3 != "") {
+      if (posVal1 === posVal2 && posVal2 === posVal3) {
+        console.log("Winner");
+        dis();
+      }
+    }
+  }
+};
+
+reset_btn.addEventListener("click", () => {
+  reset();
+});
+
+
+
+let W = window.innerWidth;
+let H = window.innerHeight;
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const maxConfettis = 150;
+const particles = [];
+
+const possibleColors = [
+  "DodgerBlue",
+  "OliveDrab",
+  "Gold",
+  "Pink",
+  "SlateBlue",
+  "LightBlue",
+  "Gold",
+  "Violet",
+  "PaleGreen",
+  "SteelBlue",
+  "SandyBrown",
+  "Chocolate",
+  "Crimson"
+];
+
+function randomFromTo(from, to) {
+  return Math.floor(Math.random() * (to - from + 1) + from);
+}
+
+function confettiParticle() {
+  this.x = Math.random() * W; // x
+  this.y = Math.random() * H - H; // y
+  this.r = randomFromTo(11, 33); // radius
+  this.d = Math.random() * maxConfettis + 11;
+  this.color =
+    possibleColors[Math.floor(Math.random() * possibleColors.length)];
+  this.tilt = Math.floor(Math.random() * 33) - 11;
+  this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
+  this.tiltAngle = 0;
+
+  this.draw = function() {
+    context.beginPath();
+    context.lineWidth = this.r / 2;
+    context.strokeStyle = this.color;
+    context.moveTo(this.x + this.tilt + this.r / 3, this.y);
+    context.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
+    return context.stroke();
+  };
+}
+
+function Draw() {
+  const results = [];
+
+  // Magical recursive functional love
+  requestAnimationFrame(Draw);
+
+  context.clearRect(0, 0, W, window.innerHeight);
+
+  for (var i = 0; i < maxConfettis; i++) {
+    results.push(particles[i].draw());
+  }
+
+  let particle = {};
+  let remainingFlakes = 0;
+  for (var i = 0; i < maxConfettis; i++) {
+    particle = particles[i];
+
+    particle.tiltAngle += particle.tiltAngleIncremental;
+    particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
+    particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
+
+    if (particle.y <= H) remainingFlakes++;
+
+    // If a confetti has fluttered out of view,
+    // bring it back to above the viewport and let if re-fall.
+    if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
+      particle.x = Math.random() * W;
+      particle.y = -30;
+      particle.tilt = Math.floor(Math.random() * 10) - 20;
+    }
+  }
+
+  return results;
+}
+
+window.addEventListener(
+  "resize",
+  function() {
+    W = window.innerWidth;
+    H = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  },
+  false
+);
+
+// Push new confetti objects to `particles[]`
+for (var i = 0; i < maxConfettis; i++) {
+  particles.push(new confettiParticle());
+}
+
+// Initialize
+canvas.width = W;
+canvas.height = H;
+Draw();
